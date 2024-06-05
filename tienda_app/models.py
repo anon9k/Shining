162db5ad323dd3be7723497c6170b8365f9a5e8b
anon9k <anon9kdev@gmail.com>
2024-06-05 18:31:47 +0200

    Modelo Item sin Imagen

--- tienda_app/models.py
+++ tienda_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model ):
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField(default=999)
+    stock = models.IntegerField(default=0)
+    opciones_categoria = [
+       ('CA', 'Camiseta'),
+       ('SU', 'Sudadera'),
+       ('PA', 'Pantalon'),
+       ('AB','Abrigo'),
+    ]
+    categoria = models.CharField(max_length=20, choices=opciones_categoria, default="Nombre prenda")
